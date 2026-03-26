040a535d520b29e30e820024b0bdbf026dc4d473
Yashu Youwaraj <amanyasu@gmail.com>
2026-03-26 10:09:45 +0530

    Core Java => Naming Convension

---
+++ 1.Core Java/NamingConvension.java
@@ -0,0 +1,20 @@
+public class NamingConvension {
+    public static void main(String[] args) {
+        
+        // Camel Case
+
+        //class and interface => Calc, Runnable , firstletter of each word is capital
+
+        // method and variable => add, getName, first letter of first word is small and first letter of each word is capital
+
+        // constant => PI, GRAVITY, all letters are capital and words are separated by underscore
+
+        // package => com.companyname.projectname, all letters are small and words are separated by dot
+
+        // file => MyFile.java, first letter of each word is capital and file extension is small
+
+        // folder => MyFolder, first letter of each word is capital
+
+        // database => my_database, all letters are small and words are separated by underscore
+    }
+}

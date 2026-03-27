a97ea06673e2d04c58f7df639d7f79b319dcbca7
Yashu Youwaraj <amanyasu@gmail.com>
2026-03-27 09:10:55 +0530

    Core Java => Packages and Access Modifiers

---
+++ 1.Core Java/packages/Demo.java
@@ -0,0 +1,13 @@
+package packages;
+
+import packages.other.A;
+import packages.other.tools.AdvCalc;
+import packages.other.tools.Calc;
+
+public class Demo {
+    public static void main(String[] args) {
+        Calc obj = new Calc();
+        AdvCalc obj2 = new AdvCalc();
+        A obj3 = new A();
+    }
+}
---
+++ 1.Core Java/packages/other/A.java
@@ -0,0 +1,5 @@
+package packages.other;
+
+public class A {
+    
+}

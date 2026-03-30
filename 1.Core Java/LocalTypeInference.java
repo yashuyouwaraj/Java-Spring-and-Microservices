3e5b34728418212f8403e7c1c8f1fb578b3a326c
Yashu Youwaraj <amanyasu@gmail.com>
2026-03-30 21:11:35 +0530

    Core Java => Local Type Inference

---
+++ 1.Core Java/LocalTypeInference.java
@@ -0,0 +1,16 @@
+class Alien{
+    
+}
+public class LocalTypeInference {
+
+    // var num=10;  //Local type inference is not allowed here
+    public static void main(String[] args) {
+        int a =10;
+        var b = 20;  //Local type inference
+
+        int nums[] = new int[10];
+        var nums2 = new int[10];  //Local type inference
+
+        var obj = new Alien();
+    }
+}

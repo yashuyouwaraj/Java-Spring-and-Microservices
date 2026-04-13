dfc5db8d209572e4b20346d9c95a3a0bbe74287e
Yashu Youwaraj <amanyasu@gmail.com>
2026-04-13 10:27:53 +0530

    Hibernate => Create and Insert

---
+++ 11.Hibernate/Hibernate_Demo/src/main/java/com/yashu/Alien.java
@@ -0,0 +1,40 @@
+package org.example;
+
+public class Alien {
+    private int aid;
+    private String aname;
+    private String tech;
+
+    @Override
+    public String toString() {
+        return "Alien{" +
+                "aid=" + aid +
+                ", aname='" + aname + '\'' +
+                ", tech='" + tech + '\'' +
+                '}';
+    }
+
+    public String getTech() {
+        return tech;
+    }
+
+    public void setTech(String tech) {
+        this.tech = tech;
+    }
+
+    public String getAname() {
+        return aname;
+    }
+
+    public void setAname(String aname) {
+        this.aname = aname;
+    }
+
+    public int getAid() {
+        return aid;
+    }
+
+    public void setAid(int aid) {
+        this.aid = aid;
+    }
+}

038b7ee22429b04abc3feb581589b85d2993cfec
Yashu Youwaraj <amanyasu@gmail.com>
2026-04-18 18:35:02 +0530

    JWT => Generating JWT Token

---
+++ 18.JWT/Generating JWT Token/src/main/java/com/yashu/SpringSecEx/service/JWTService.java
@@ -0,0 +1,51 @@
+package com.yashu.SpringSecEx.service;
+
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class JWTService {
+    private String secretKey = "";
+
+    public JWTService() {
+
+        try {
+            KeyGenerator keyGen = KeyGenerator.getInstance("HmacSHA256");
+            SecretKey sk = keyGen.generateKey();
+            secretKey = Base64.getEncoder().encodeToString(sk.getEncoded());
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String generateToken(String username) {
+        Map<String, Object> claim = new HashMap<>();
+        return Jwts.builder()
+                .claims()
+                .add(claim)
+                .subject(username)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + 60 * 60 * 30))
+                .and()
+                .signWith(getKey())
+                .compact();
+    }
+
+    private Key getKey(){
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+}
